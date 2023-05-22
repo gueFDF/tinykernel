6c6d6758f41c085b6acdb913f1e07dd2b8360e38
gueFDF <3237455241@qq.com>
2023-05-22 23:47:54 +0800

    自定义中断

--- kernel/main.c
+++ kernel/main.c
@@ -1,12 +1,8 @@
 #include "../lib/kernel/console.h"
 int main(void) {
-  console_clear();
+ // console_clear();
   console_write("hello world1\n");
-  console_write("hello world2\n");
-
-  console_write("hello world3\n");
-
-  console_write("hello world4\n");
+ 
 
   while (1)
     ;

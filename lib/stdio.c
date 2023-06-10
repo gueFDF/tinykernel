18198eddc369d5a1d827d7b674dbad1c3bbae907
gueFDF <3237455241@qq.com>
2023-06-10 21:06:23 +0800

    完善staio.h库

--- lib/stdio.c
+++ lib/stdio.c
@@ -30,6 +30,7 @@ uint32_t vsprintf(char* str, const char* format, va_list ap) {
   char index_char = *index_ptr;
 
   int32_t arg_int;
+  char* arg_str;
   while (index_char) {
     if (index_char != '%') {
       *(buf_ptr++) = index_char;
@@ -38,6 +39,28 @@ uint32_t vsprintf(char* str, const char* format, va_list ap) {
     }
     index_char = *(++index_ptr);  // 得到%后面的字符
     switch (index_char) {
+      case 's':
+        arg_str = va_arg(ap, char*);
+        strcpy(buf_ptr, arg_str);
+        buf_ptr += strlen(arg_str);
+        index_char = *(++index_ptr);
+        break;
+
+      case 'c':
+        *(buf_ptr++) = va_arg(ap, char);
+        index_char = *(++index_ptr);
+        break;
+
+      case 'd':
+        arg_int = va_arg(ap, int);
+        if (arg_int < 0) {
+          arg_int = 0 - arg_int;
+          *buf_ptr++ = '-';
+        }
+        itoa(arg_int, &buf_ptr, 10);
+        index_char = *(++index_ptr);
+        break;
+
       case 'x':
         arg_int = va_arg(ap, int);
         itoa(arg_int, &buf_ptr, 16);
@@ -46,6 +69,16 @@ uint32_t vsprintf(char* str, const char* format, va_list ap) {
     }
   }
   return strlen(str);
+}
+
+/*sprintf*/
+uint32_t sprintf(char* buf, const char* format, ...) {
+  va_list args;
+  uint32_t retval;
+  va_start(args, format);
+  retval = vsprintf(buf, format, args);
+  va_end(args);
+  return retval;
 }
 
 /*格式化输出字符串format*/
--- lib/stdio.h
+++ lib/stdio.h
@@ -3,6 +3,7 @@
 #include "types.h"
 typedef char* va_list;
 
+uint32_t vsprintf(char* str, const char* format, va_list ap);
 uint32_t printf(const char* format, ...);
-uint32_t printf(const char* format, ...);
+uint32_t sprintf(char* buf, const char* format, ...);
 #endif /* LIB_STDIO */
